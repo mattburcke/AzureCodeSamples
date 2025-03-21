204a4ca64a1ad5196fd51b6e0b5486f368c5b041
Matt Burcke <mattburcke@microsoft.com>
2025-03-21 11:43:06 -0400

    commit

---
+++ Azure-Automation/ps-loadvirtualmachine.ps1
@@ -0,0 +1,56 @@
+# Authenticate with managed identity (if running in Azure Automation)
+Connect-AzAccount -Identity
+
+# Set variables
+$subscriptionId = "0d77cf6e-ce0a-4303-8ccb-a5f997fdc950"
+$resourceGroupName = "rg-redhatautomation"
+$vmName = "testvm"
+$location = "canadacentral"
+$galleryName = "gallery"
+$imageDefinitionName = "definition-main"
+$imageVersion = "0.0.1"
+$adminUsername = "mburckeadmin"
+$adminPassword = "" | ConvertTo-SecureString -AsPlainText -Force
+$vmSize = "Standard_B2as_v2"
+$virtualnetworkName = "vnet-test"
+
+# Select the subscription
+Select-AzSubscription -SubscriptionId $subscriptionId
+
+# Check if the VM exists
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -ErrorAction SilentlyContinue
+
+if ($vm) {
+    # Get the NIC and Disk associated with the VM
+    $nicId = $vm.NetworkProfile.NetworkInterfaces.Id
+    $osDiskName = $vm.StorageProfile.OsDisk.Name
+
+    # Delete the VM
+    Remove-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
+
+    # Delete the NIC
+    $nicName = (Get-AzResource -ResourceId $nicId).Name
+    Remove-AzNetworkInterface -ResourceGroupName $resourceGroupName -Name $nicName -Force
+
+    # Delete the OS Disk
+    Remove-AzDisk -ResourceGroupName $resourceGroupName -DiskName $osDiskName -Force
+
+    Write-Output "Deleted existing VM, NIC, and Disk."
+} else {
+    Write-Output "VM does not exist."
+}
+
+# Get the image version
+$image = Get-AzGalleryImageVersion -ResourceGroupName $resourceGroupName -GalleryName $galleryName -GalleryImageDefinitionName $imageDefinitionName -Name $imageVersion
+
+# Create the VM configuration
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize $vmSize -SecurityType "TrustedLaunch" |
+  Set-AzVMOperatingSystem -Linux -ComputerName $vmName -Credential (New-Object PSCredential ($adminUsername, $adminPassword)) |
+    Set-AzVMSourceImage -Id $image.Id |
+    Add-AzVMNetworkInterface -Id (New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Name "$vmName-nic" -Location $location -SubnetId (Get-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name "$virtualnetworkName").Subnets[0].Id).Id
+
+# Create the VM
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
+
+# Start the VM
+# Start-AzVM -ResourceGroupName "$resourceGroupName" -Name "$vmName"
